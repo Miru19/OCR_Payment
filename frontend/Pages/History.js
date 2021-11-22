eec0988937c6a77752ed5db1a7a60c6940940a76
Miru19 <56609347+Miru19@users.noreply.github.com>
2021-11-22 15:05:23 +0200

    Implement view for tabs

--- frontend/Pages/History.js
+++ frontend/Pages/History.js
@@ -1,27 +1,38 @@
 import React from "react";
-import { List, Text } from "react-native-paper";
+import { List } from "react-native-paper";
 import axiosInstance from "../api/instance";
+import { StyleSheet, Image, View, Text } from 'react-native';
 
-export class History extends React.Component{
-    constructor(props){
+const image = require('../assets/emptyState.png')
+export class History extends React.Component {
+    constructor(props) {
         super(props);
     }
 
     async readPayments() {
         const response = await axiosInstance.get("/users/all");
         const payments = [];
-        for(let index=0;index<response.data.length;index++){
+        for (let index = 0; index < response.data.length; index++) {
             payments.push(<List.Item></List.Item>);
         }
         console.log(payments);
         return (payments);
     }
 
-    render(){
-        return(
-            <Text>
-   
-            </Text>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={image} />
+                <Text style={{fontSize:15, fontWeight:'800', color:"#29356d"}}>No payments yet.</Text>
+            </View>
         );
     }
 }
+
+const styles= StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
--- frontend/Pages/Pay.js
+++ frontend/Pages/Pay.js
@@ -1,15 +1,78 @@
 import React from "react";
-import { Text } from "react-native-paper";
+import { StyleSheet, View, Text } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { TextInput } from "react-native-paper";
+import { CustomButton } from "../Components/CustomButton";
 
-export class Pay extends React.Component{
-    constructor(props){
+export class Pay extends React.Component {
+    constructor(props) {
         super(props);
-        
+        this.state = {
+            citiesOpen: false,
+            areasOpen: false
+        }
+
+        this.setCitiesOpen = this.setCitiesOpen.bind(this);
+        this.setAreasOpen = this.setAreasOpen.bind(this);
     }
 
-    render(){
-        return(
-            <Text>Hello</Text>
+    citiesList = [
+        { label: 'Brasov', value: 'Brasov' },
+        { label: 'Cluj Napoca', value: 'Cluj Napoca' },
+        { label: 'Bucuresti', value: 'Bucuresti' },
+    ]
+
+
+    areasList = [
+        { label: 'Area 0', value: '0' },
+        { label: 'Area 1', value: '1' },
+        { label: 'Area 2', value: '2' },
+    ]
+
+    setCitiesOpen() {
+        this.setState({ citiesOpen: !this.state.citiesOpen });
+    }
+
+    setAreasOpen() {
+        this.setState({ areasOpen: !this.state.areasOpen });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <DropDownPicker
+                    open={this.state.citiesOpen}
+                    items={this.citiesList}
+                    setOpen={this.setCitiesOpen}
+
+                />
+                <DropDownPicker
+                    open={this.state.areasOpen}
+                    items={this.areasList}
+                    setOpen={this.setAreasOpen}
+                />
+                <CustomButton buttonText="Scan Plate" color="#29356d" fontColor="#ffffff" />
+                <Text> or </Text>
+                <TextInput mode="outlined" label="Plate Number" style={styles.input} />
+
+                <CustomButton buttonText="Pay" color="#29356d" fontColor="#ffffff" />
+
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: '80%'
+    },
+    input: {
+        width: '80%',
+        margin: 15,
+        borderRadius: 15,
+    },
+});
